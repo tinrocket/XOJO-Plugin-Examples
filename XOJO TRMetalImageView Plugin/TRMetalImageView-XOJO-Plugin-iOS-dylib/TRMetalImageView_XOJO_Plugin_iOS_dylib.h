99a1c5392c54ffd30d8af48358a4d0d2ff18bc7a
John Balestrieri <mrjohn@tinrocket.com>
2024-07-01 09:33:17 -0400

    WIP new dylib target for iOS

---
+++ XOJO TRMetalImageView Plugin/TRMetalImageView-XOJO-Plugin-iOS-dylib/TRMetalImageView_XOJO_Plugin_iOS_dylib.h
@@ -0,0 +1,12 @@
+//
+//  TRMetalImageView_XOJO_Plugin_iOS_dylib.h
+//  TRMetalImageView-XOJO-Plugin-iOS-dylib
+//
+//  Created by John Balestrieri on 7/1/24.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TRMetalImageView_XOJO_Plugin_iOS_dylib : NSObject
+
+@end
